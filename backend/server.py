7460fb08c37f91d56acba4cb5bf8b5f19b422e6c
Zichenglong Wang <zw84@illinois.edu>
2023-11-23 17:51:41 -0600

    add template of backend

--- backend/server.py
+++ backend/server.py
@@ -13,20 +13,21 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return 'Hello, World!'
 
 
 # Define a JSON API endpoint
-@app.route('/api/get-rating', methods=['GET'])
+@app.route('/api/rating', methods=['GET'])
 def get_rating():
+    ratee_data = request.get_json()
     data = {'message': 'This is data from the backend!'}
     return jsonify(data)
 
-@app.route('/api/sign_up', methods=['POST'])
-def sign_up():
+
+@app.route('/api/registration', methods=['POST'])
+def register():
     user_data = request.get_json()
     email = user_data["email"]
     password = user_data["password"]
@@ -37,17 +38,44 @@ def sign_up():
     #Insert to mysql
 
 
-@app.route('/api/login', methods=['POST'])
-def sign_up():
+@app.route('/api/login', methods=['GET'])
+def login():
     user_data = request.get_json()
 
     #validate whether the username and password pair is valid
     #Select password from User table using email
 
 
+@app.route('/api/table', methods=['GET'])
+def query_table():
+    filters = request.get_json()
+
+    #Get table infor based on filters
 
 
 
+
+@app.route('/api/leaderboard', methods=['GET'])
+def get_leaderboard():
+    data = request.get_json()
+
+    # Get country information with count of metals and orders
+
+
+@app.route('/api/ratee', methods=['GET'])
+def get_leaderboard():
+    data = request.get_json()
+    name = data["name"]
+    type = data["type"]
+
+    # Get ratee information
+
+
+@app.route('/api/comment', methods=['POST'])
+def post_comment():
+    data = request.get_json()
+    content = data["content"]
+    email = data["email"]
 
 if __name__ == "__main__":
     app.run()
