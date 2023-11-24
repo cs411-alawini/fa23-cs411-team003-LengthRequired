7cd6f1cd58070273e7bdd7ea1ca18856f3b2ecb5
Zichenglong Wang <zw84@illinois.edu>
2023-11-24 17:38:28 -0600

    add config to database

--- backend/server.py
+++ backend/server.py
@@ -5,12 +5,36 @@ import io
 import warnings
 import threading
 import subprocess
-
+import pickle
 from flask import Flask, request, jsonify, make_response, Response
 from flask_cors import CORS, cross_origin
+import mysql.connector
 
+
+pickle_file_path = 'config.pickle'
+
+# Load the dictionary from the pickle file
+with open(pickle_file_path, 'rb') as file:
+    loaded_dict = pickle.load(file)
+
+USER = loaded_dict["user"]
+PASSWORD = loaded_dict["password"]
+HOST = loaded_dict["host"]
 
 app = Flask(__name__)
+
+
+db_config = {
+    "user": USER,
+    "password": PASSWORD,
+    "host": HOST,
+    "database": "db",
+}
+
+# Establish a connection to the database
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -76,6 +100,7 @@ def post_comment():
     data = request.get_json()
     content = data["content"]
     email = data["email"]
+
 
 if __name__ == "__main__":
     app.run()
