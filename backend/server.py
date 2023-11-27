91fd38a0c47789f37979ee886d20516d4fb85a98
Blizzard416 <wentao.zhou@mail.utoronto.ca>
2023-11-26 22:39:35 -0600

    Added stored procedure PlayerRank endpoint

--- backend/server.py
+++ backend/server.py
@@ -357,7 +357,8 @@ def get_athlete_by_country():
 
     except Exception as e:
         return {"error": str(e)}
-    
+
+
 @app.route('/api/medal', methods=['GET'])
 def get_medal():
     """
@@ -388,6 +389,65 @@ def get_medal():
         query = f"SELECT * FROM Country c ORDER BY c.Ranks"
         cursor.execute(query)
         return {'data': [dict(zip(cursor.column_names, row)) for row in cursor.fetchall()]}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+# Retrieve the result from the stored procedure
+"""
+DELIMITER //
+CREATE PROCEDURE PlayerRank()
+BEGIN
+	DECLARE varRateeId INT;
+	DECLARE varName VARCHAR(50);
+	DECLARE varScore DOUBLE;
+	DECLARE varRank VARCHAR(2);
+	DECLARE done BOOLEAN DEFAULT FALSE;
+	DECLARE cur CURSOR FOR (SELECT RateeId, Name, (IFNULL(Rating/Average, 0)*0.8+CommentCount*0.2) AS Score FROM RatingComment NATURAL JOIN DisciplineAverage);
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+	DROP TEMPORARY TABLE IF EXISTS RatingComment; 
+DROP TEMPORARY TABLE IF EXISTS DisciplineAverage; 
+	CREATE TEMPORARY TABLE RatingComment AS
+	(SELECT RateeId, Name, CommentCount, (SumofRating/NULLIF(NumofRating, 0)) AS Rating, Discipline FROM Athlete NATURAL JOIN Ratee NATURAL JOIN (SELECT Target AS RateeId, COUNT(*) AS CommentCount FROM Comment GROUP BY Target) AS Temp);
+	CREATE TEMPORARY TABLE DisciplineAverage AS
+	(SELECT Discipline, AVG(SumofRating/NULLIF(NumofRating, 0)) AS Average FROM Ratee NATURAL JOIN Athlete GROUP BY Discipline);
+	DROP TABLE IF EXISTS FinalTable;
+	CREATE TABLE FinalTable (RateeId INT PRIMARY KEY, Name VARCHAR(50), PlayerRank VARCHAR(2), Score DOUBLE);
+	OPEN cur;
+		Cloop: LOOP
+		FETCH cur INTO varRateeId, varName, varScore;
+	IF done THEN
+		LEAVE cloop;
+	END IF;
+
+	IF varScore >= 5.1 THEN
+		SET varRank = 'A';
+	ELSEIF varScore >= 2.8 THEN
+		SET varRank = 'B';
+ELSE
+	SET varRank = 'C';
+END IF;
+
+INSERT INTO FinalTable VALUE (varRateeId, varName, varRank, varScore);
+END LOOP cloop;
+CLOSE cur;
+
+	SELECT * FROM FinalTable ORDER BY PlayerRank;
+END//
+DELIMITER ;
+
+"""
+
+
+@app.route('/api/player-rank', methods=['GET'])
+def player_rank():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        dict_cursor = conn.cursor(dictionary=True)
+        dict_cursor.callproc("PlayerRank")
+        results = next(dict_cursor.stored_results()).fetchall()
+        return {'data': results}
 
     except Exception as e:
         return {"error": str(e)}
