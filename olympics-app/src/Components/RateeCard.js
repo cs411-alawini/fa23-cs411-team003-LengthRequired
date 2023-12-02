a0f8810009651c0dd426765453eebcf5dcfb5d23
William <lyf1436557019@qq.com>
2023-12-02 15:15:36 -0600

    refresh comment, grid athlete

--- olympics-app/src/Components/RateeCard.js
+++ olympics-app/src/Components/RateeCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../StyleSheets/RateeCard.css';
 
 function RateeCard({ RateeId, Name, Country, Discipline }) {
     const navigate = useNavigate();
@@ -13,9 +14,8 @@ function RateeCard({ RateeId, Name, Country, Discipline }) {
             <h2>
                 <span onClick={navigateToRateePage}>{Name}</span>
             </h2>
-            <p>{RateeId}</p>
-            <p>{Country}</p>
-            <p>{Discipline}</p>
+            <p>Country: {Country}</p>
+            <p>Discipline: {Discipline}</p>
         </div>
     );
 }
